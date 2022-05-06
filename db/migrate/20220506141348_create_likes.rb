7a6646bb11639156224779ac95c28f288f388ba7
Mphatso Lichapa <lichapau@gmail.com>
2022-05-06 16:20:28 +0200

    Created_data_model

--- db/migrate/20220506141348_create_likes.rb
+++ db/migrate/20220506141348_create_likes.rb
@@ -1,6 +1,6 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
-    create_table :likes do |t|
+    create_table :likes do |t| 
       t.references :author, index:true, foreign_key: {to_table: :users}
       t.references :post, null: false, foreign_key: true
 
