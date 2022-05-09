49fb85cc342d04cb772625def4e60d7a745d9bd2
Mphatso Lichapa <lichapau@gmail.com>
2022-05-09 21:09:55 +0200

    Added method current in application controller

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,10 @@
 class Comment < ApplicationRecord
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: 'User'
   belongs_to :post
 
   def update_comments_counter
-    counter = Comment.where(post_id: self.post_id).count
-    post = Post.find_by(id: self.post_id)
+    counter = Comment.where(post_id:).count
+    post = Post.find_by(id: post_id)
     post.update(comments_counter: counter)
   end
-
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,15 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
 
   def user_post_counter
-    counter = Post.where(author_id: self.author_id).count 
-    user = User.find_by_id(id: self.author_id)
+    counter = Post.where(author_id:).count
+    user = User.find_by_id(id: author_id)
     user.update(posts_counter: counter)
   end
 
   def recent_comments
-    Comment.where(post_id: self.id).order(created_at: :desc).limit(5)
+    Comment.where(post_id: id).order(created_at: :desc).limit(5)
   end
 end
