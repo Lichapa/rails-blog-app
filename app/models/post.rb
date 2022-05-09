153ee955dd783809e408b397c50e568fa533b8b5
Mphatso Lichapa <lichapau@gmail.com>
2022-05-09 20:26:44 +0200

    Added method for updating user posts counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,9 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
-
+  def user_post_counter
+    counter = Post.where(author_id: self.author_id).count 
+    user = User.find_by_id(id: self.author_id)
+    user.update(posts_counter: counter)
+  end
 end
