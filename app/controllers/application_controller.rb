1a235f441dae8a867a9741acf6cde4888a8c0526
Mphatso Lichapa <lichapau@gmail.com>
2022-05-16 21:32:06 +0200

    Added home page and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
- 
+  class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email bio photo password])
+      devise_parameter_sanitizer.permit(:sign_in, keys: %i[email password])
+    end
+  end
 end
