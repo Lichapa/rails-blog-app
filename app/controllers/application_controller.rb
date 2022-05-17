e22f8a5d66c084c97cfeb2aa2db8f1a4f26fd743
Mphatso Lichapa <lichapau@gmail.com>
2022-05-17 09:00:57 +0200

    configured signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
-  class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+  protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email bio photo password])
-      devise_parameter_sanitizer.permit(:sign_in, keys: %i[email password])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:name, :email, :bio, :photo, :password, :posts_counter)
+    end
+
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:email, :password, :remember_me)
     end
   end
 end
